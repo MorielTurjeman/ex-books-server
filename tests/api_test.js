5eb9d0cdac9aac82f5f9b4105ff4193a59dbcc05
Moriel Turjeman <moriel99@pop-os.localdomain>
2020-12-26 23:47:26 +0200

    added testing code

---
+++ tests/api_test.js
@@ -0,0 +1,11 @@
+const axios=require("axios");
+
+
+axios.get('https://openlibrary.org/books/OL7353617M.json')
+  .then(response => {
+    console.log(response);
+   
+  })
+  .catch(error => {
+    console.log(error);
+  });
