f3f5a7e23ebd8a895027b1fd539076e2d8f84e35
Moriel Turjeman <moriel99@pop-os.localdomain>
2021-02-23 01:25:41 +0200

    swap almost done

--- controllers/swap.ctrl.js
+++ controllers/swap.ctrl.js
@@ -1,6 +1,5 @@
 const Swap = require('../models/swap');
 const User = require('../models/user');
-
 exports.swapDbcontroller = {
     getSwaps(req, res) {
         let filter = {};
@@ -34,18 +33,37 @@ exports.swapDbcontroller = {
         const swap= req.body;
         console.log(swap);
         if (swap.user_id1 != req.user._id)
+        {
             res.status(400).json("No permission");
+            return;
+        }
         
+        if (!(swap.book_id1 || swap.book_id2))
+        {
+            res.status(400).json("No books requested for swap");
+            return;
+        }
+
         const newSwap= new Swap(swap);
         const isBookId1 = User.findById(swap.user_id1).then(user => {
-            console.log(user.books);
+            if (!swap.book_id1) return true;
             if (user.books.includes(swap.book_id1))
                 return true;
             else
                 return false;
+        });
+        const isBookId2 = User.findById(swap.user_id2).then(user => {
+            if (!swap.book_id2) return true;
+            
+            if (user.books.includes(swap.book_id2))
+                return true;
+            else
+                return false;
         })
-        .then(bookValid => {
-            if (bookValid)
+
+        Promise.all([isBookId1, isBookId2])
+        .then(([book1_valid, book2_valid]) => {
+            if (book1_valid && book2_valid)
             {
                 return newSwap.save()        
             }
@@ -68,14 +86,15 @@ exports.swapDbcontroller = {
     updateSwap(req, res){
         user_id = req.user._id;
         swap_id=req.params.id;
-
-        Swap.findById(swap_id)
+        Swap.findOne({$and: [{_id: swap_id}, {$or: [{user_id1: user_id}, {user_id2: user_id}]} ]})
             .then(async swap=> {
-                if(swap.user_id1 != user_id || swap.user_id2 !=user_id){
+                if(!swap){
                     res.status(403).send("No permission to update swap");
                 }
                 else{
                     swap.swap_status= req.body.swap_status;
+                    swap.book_id1 = req.body.book_id1;
+                    swap.book_id2 = req.body.book_id2;
                     await swap.save();
                     res.json(swap);
                 }   
@@ -89,7 +108,7 @@ exports.swapDbcontroller = {
 
     deleteSwap(req, res){
         user_id = req.user._id;
-        swap_id=req.params.id;
+        swap_id = req.params.id;
 
         Swap.findById(swap_id)
             .then(async swap=> {
